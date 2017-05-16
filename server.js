9d79ba2ed1527c76b787bcfa92f54e2bffab26fe
“Vilde <vilde@vevatne.no>
2017-05-16 10:45:30 -0700

    ADds hbs

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getFullYear', () => {
+  return new Date().getFullYear()
+});
+
 app.get('/', (req, res) => {
-  // res.send('<h1>hello Express</h1>');
   res.send({
     name: 'Vilde',
     likes: [
-      'Carbs',
+      'Fat',
       'Men'
     ]
   })
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page')
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    pageText: 'Habababa'
+  });
 });
 
 app.get('/bad', (req, res) => {
